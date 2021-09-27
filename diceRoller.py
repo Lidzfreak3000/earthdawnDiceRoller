b46363830a8ebd2e6468504578536c9f3f7a271a
Lidzfreak3000 <j.r.nerdy@hotmail.com>
2021-09-27 11:52:22 -0400

    initial commit

---
+++ diceRoller.py
@@ -0,0 +1,45 @@
+# bot.py
+import os
+import discord
+import d20
+
+from myDicts import steps, help
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(aliases=['step', 's'])
+async def send_roll(ctx, *args):
+    response = 'Unkown Error Occured.'
+    karma = {
+        "k",
+        "karma"
+    }
+
+    try:
+        num = int(args[0])
+
+        if len(args) >= 2 and args[1]!=None:
+            strArg2 = str(args[1])
+        else:
+            strArg2 = ''
+
+        if karma.__contains__(strArg2.lower()):
+            response = d20.roll(steps[args[0]] + '+1d6e6')
+        else:
+            response = d20.roll(steps[args[0]])
+
+    except Exception as e:
+        response = str(e) + '\n\nError. Please try again using the format "!step {number} {k/karma}" Ex: "!step 8"'
+    await ctx.send(response)
+
+@bot.help_command()
+# async def send_help(ctx, *args):
+#     response = help[1]
+#     await ctx.send(response)
+
+bot.run(TOKEN)
